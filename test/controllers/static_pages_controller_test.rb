eefb21d2b9ba2c2b62f60c051ecaaa2d8387ce95
Drew Conly <atconly@gmail.com>
2016-04-07 10:22:33 -0600

    finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,7 +13,12 @@ class StaticPagesControllerTest < ActionController::TestCase
 
   test "should get about" do
     get :about
-    assert_response :success 
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
   end
 
 end
